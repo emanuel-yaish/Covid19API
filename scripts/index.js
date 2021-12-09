7449aa8839f634b24a4fce583184d825bf59dfff
Emanuel Yaish <emanuelyaish@gmail.com>
2021-12-09 12:53:45 +0200

    covid 19 start files

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const covid19 = {
+  // properties
+  continents: [],
+  selectedContinent: "",
+  continentsApi: {},
+
+  // methods
+
+  getContinentCountries() {},
+
+  initEvents() {
+    // this.clickStartScreen();
+    // this.clickOnTool();
+    // this.clickOnInventory();
+    // this.clickOnGameBoard();
+    // this.ResetGameBoard();
+  },
+
+  init() {
+    // this.startButton = document.querySelector(".start-button");
+    // this.startScreen = document.querySelector(".start-screen");
+    // this.gameBoard = document.querySelector(".game-board");
+    // this.inventoryElement = document.querySelector(".inventory");
+    // this.resetButton = document.querySelector(".reset-button");
+    // this.sideNavButtons = document.querySelectorAll(".side-nav-button");
+    // this.initEvents();
+    // this.draw();
+  },
+};
+
+covid19.init();
